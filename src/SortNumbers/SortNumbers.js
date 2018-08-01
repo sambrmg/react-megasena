ec424703202e92df99ba8c559efc311c240640f2
Guilherme <sambrmg@gmail.com>
2018-07-31 23:15:28 -0300

    registrar e iniciar sorteio

--- src/SortNumbers/SortNumbers.js
+++ src/SortNumbers/SortNumbers.js
@@ -2,24 +2,28 @@ import React, { Component } from 'react';
 
 const _verificarAcerto = (i, numeros) => {
     if( numeros.indexOf(i) !== -1 ){
-        return 'acerto'
+        return true
     }
-    return ''
+    return false
 }
 export default class SortNumbers extends Component{
     constructor(props) {
         super(props);
         this.state = {
+            valores: [],
             numeros: [],
             jogos: [ 
                 [1,2,3,4,5,6], 
                 [7,8,9,10,11,12],
                 [7,18,24,32,44,56] ]
         }
+        // this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleIniciarSorteio = this.handleIniciarSorteio.bind(this);
     }
-    componentWillMount(){
+    handleIniciarSorteio(){
         const _self = this;
-        
+        _self.state.numeros = [];
         var maxInterval = 6;
 
         function getRandomArbitrary(min, max) {
@@ -27,7 +31,7 @@ export default class SortNumbers extends Component{
         }
 
         var interval = setInterval(function(){
-            let numRandom = getRandomArbitrary(1, 60);
+            let numRandom = getRandomArbitrary(1, 20);
             if( _self.state.numeros.indexOf( numRandom ) === -1 ){
                 _self.setState({
                     numeros: [..._self.state.numeros, numRandom]
@@ -41,17 +45,42 @@ export default class SortNumbers extends Component{
         }, 1000);
         
     }
+    
+    handleClick(){
+        this.setState({jogos: [...this.state.jogos, [
+            this.state.valores.p1,
+            this.state.valores.p2,
+            this.state.valores.p3,
+            this.state.valores.p4,
+            this.state.valores.p5,
+            this.state.valores.p6] ] });
+    }
+    handleChange(propertyName, event) {
+        const valores = this.state.valores;
+        valores[propertyName] = event.target.value;
+        this.setState({ propertyName: event.target.value });
+    }
     render(props){
         const _self = this;
 
         return (
             <div className="megasena">
+                <div className="fm-inline">
+                    <input maxLength="2" onChange={this.handleChange.bind(this, 'p1')}/>
+                    <input maxLength="2" onChange={this.handleChange.bind(this, 'p2')}/>
+                    <input maxLength="2" onChange={this.handleChange.bind(this, 'p3')}/>
+                    <input maxLength="2" onChange={this.handleChange.bind(this, 'p4')}/>
+                    <input maxLength="2" onChange={this.handleChange.bind(this, 'p5')}/>
+                    <input maxLength="2" onChange={this.handleChange.bind(this, 'p6')}/>
+                    <button onClick={this.handleClick}>Registrar</button>
+                    <button onClick={this.handleIniciarSorteio}>Iniciar Sorteio</button>
+                </div>
                 <p>Meus jogos</p>
                 {_self.state.jogos.map( (el) => {
                     return(
                     <ul key={el}>
                         {el.map(elm => {
-                            return (<li key={elm} className={`${ _verificarAcerto(elm, _self.state.numeros) }`}>{elm}</li>)
+                            return (<li key={elm} className={`${ _verificarAcerto(elm, _self.state.numeros)? 'acerto': '' }`}>{elm}</li>)
                         })}
                     </ul>)
                 })}
